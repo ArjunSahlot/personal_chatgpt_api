d06f65b9f64895e5543e75bd00010c4d576675e1
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-10-03 21:17:39 -0700

    update

--- server.js
+++ server.js
@@ -76,5 +76,5 @@ app.post("/chat", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+	logger.info(`Server is running on port ${PORT}`);
 });
